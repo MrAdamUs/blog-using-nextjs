8dd359d54a9a26ed23e7c747a10db900caf2be24
Adam Mawlawi <adam@mradam.us>
2021-08-05 14:43:25 -0500

    fetch data from files

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Layout from '../components/Layout'
 
 export default function HomePage() {
@@ -6,4 +9,25 @@ export default function HomePage() {
       <h1>Hello world</h1>
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+  const files  = fs.readdirSync(path.join('posts'))
+
+   const posts = files.map(fileName => {
+     const slug = fileName.replace('.md', '')
+
+     const markdownWithMeta = fs.readFileSync(path.join('posts', fileName), 'utf-8')
+ const {data: frontmatter} = matter(markdownWithMeta)
+ 
+     return {
+       slug,
+       frontmatter
+     }
+   })
+  console.log(posts)
+  return {
+    props: {}
+  }
 }
