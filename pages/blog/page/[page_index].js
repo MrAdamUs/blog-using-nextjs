88adb215e084bb2f12d4a60383249c4ceb96a409
Adam <adam@mradam.us>
2021-08-06 21:06:31 -0500

    done bulding Pagination component

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import Post from '../../../components/Post';
 import { sortByDate } from '../../../utils';
 import { POST_PER_PAGE } from '../../../config';
+import Pagination from '../../../components/Pagination';
 
 export default function BlogPage({ posts, numPages, currentPage }) {
   return (
@@ -16,6 +17,8 @@ export default function BlogPage({ posts, numPages, currentPage }) {
           <Post key={index} post={post} />
         ))}
       </div>
+
+      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   );
 }
